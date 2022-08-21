0989b88153aa0a5a12eb15a3f0223b36650936b2
Nguyễn Văn Tú <86882000+nguyenvantu11052002@users.noreply.github.com>
2022-08-21 22:40:51 +0700

    Create PY01028 - TÁCH TỪ.py

---
+++ PY01028 - TÁCH TỪ.py
@@ -0,0 +1,16 @@
+import math
+
+def solve() :
+    s = str(input())
+    res  = s.split(" ")
+    for i in res:
+        print(i)
+
+t = 1
+#t = int(input())
+for i in range (t) : solve()
+
+# 3
+# 1539786
+# 1234789
+# 8686
