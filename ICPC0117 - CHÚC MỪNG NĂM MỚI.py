a4656bee98e1277e2f93d0825827909794723bcf
Nguyễn Văn Tú <86882000+nguyenvantu11052002@users.noreply.github.com>
2022-09-07 20:49:54 +0700

    Create ICPC0117 - CHÚC MỪNG NĂM MỚI.py

---
+++ ICPC0117 - CHÚC MỪNG NĂM MỚI.py
@@ -0,0 +1,19 @@
+import math
+
+def TC () :
+    f = set()
+    n = int(input())
+    for i in range(n) :
+        s = input()
+        f.add(s)
+    print(len(f))
+
+t = 1
+#t = int(input())
+for i in range (t) : TC()
+
+# 4
+# CHUC MUNG NAM MOI
+# HAPPY NEW YEAR
+# CHUC MUNG TUOI MOI
+# CHUC MUNG NAM MOI
