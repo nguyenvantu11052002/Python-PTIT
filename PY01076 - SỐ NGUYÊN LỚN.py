131833294d744b8f057592585510fa47b09c2ca4
Nguyễn Văn Tú <86882000+nguyenvantu11052002@users.noreply.github.com>
2022-10-09 10:58:21 +0700

    Create PY01076 - SỐ NGUYÊN LỚN.py

---
+++ PY01076 - SỐ NGUYÊN LỚN.py
@@ -0,0 +1,14 @@
+import math
+
+def TC () :
+    a = int(input())
+    b = int(input())
+    print(math.gcd(a, b))
+
+t = 1
+t = int(input())
+for i in range (t) : TC()
+
+# 1
+# 1221
+# 1234567891011121314151617181920212223242526272829
