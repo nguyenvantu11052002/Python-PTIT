91fd48db7f9efb462010af31dcfe2d48332a5dc0
Nguyễn Văn Tú <86882000+nguyenvantu11052002@users.noreply.github.com>
2022-10-09 10:03:12 +0700

    Create PY01060 - TÍCH CHỮ SỐ - TỔNG CHỮ SỐ.py

---
+++ PY01060 - TÍCH CHỮ SỐ - TỔNG CHỮ SỐ.py
@@ -0,0 +1,38 @@
+import math
+
+def tong(s):
+    res = 0
+    for i in range(1, len(s), 2):
+        res += int(s[i])
+    return res
+
+def solve(s):
+    res = 1
+    for i in range(0, len(s), 2):
+        if int(s[i]) != 0:
+            res *= int(s[i])
+    return res
+
+def tich(s):
+    ok = False
+    for i in range(0, len(s), 2):
+        if int(s[i]) != 0:
+            ok = True
+            break
+    if ok == False:
+        return 0
+    else:
+        return solve(s)
+
+def TC () :
+    s = input()
+    print(tich(s), tong(s))
+
+t = 1
+t = int(input())
+for i in range (t) : TC()
+
+# 3
+# 12345678
+# 20000
+# 22334455667788
