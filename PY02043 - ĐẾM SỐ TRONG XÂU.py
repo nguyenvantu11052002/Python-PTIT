3bacadd6f37a19d2491d0b8f04b2e8eb85657506
Nguyễn Văn Tú <86882000+nguyenvantu11052002@users.noreply.github.com>
2022-12-02 10:48:50 +0700

    Create PY02043 - ĐẾM SỐ TRONG XÂU.py

---
+++ PY02043 - ĐẾM SỐ TRONG XÂU.py
@@ -0,0 +1,20 @@
+import math
+
+def TC():
+    s1 = input()
+    s2 = input()
+    cnt = s1.count(s2)
+    print(cnt)
+
+
+t = 1
+t = int(input())
+for i in range(t): TC()
+
+# 2
+# 1212121112211221121
+# 121
+# 2222222222322292
+# 2222
+
+
