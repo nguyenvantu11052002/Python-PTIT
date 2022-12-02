c950395698f134b5490f1a11ec4a4611c708e187
Nguyễn Văn Tú <86882000+nguyenvantu11052002@users.noreply.github.com>
2022-12-02 11:34:58 +0700

    Create PY02032 - LIỆT KÊ CÁC SỐ CÓ HAI CHỮ SỐ TĂNG DẦN.py

---
+++ PY02032 - LIỆT KÊ CÁC SỐ CÓ HAI CHỮ SỐ TĂNG DẦN.py
@@ -0,0 +1,27 @@
+import math
+
+def TC():
+    s = input()
+    res = ""
+    if len(s) % 2 != 0:
+        for i in range(len(s) - 1):
+            res += s[i]
+    else:
+        res = s
+    a = []
+    for i in range(0, len(res), 2): # 1 2 3 4 5
+        x = int(res[i]) * 10 + int(res[i + 1])
+        if not(x in a):
+            a.append(x)
+    a = sorted(a)
+    for i in a:
+        print(i, end = " ")
+
+t = 1
+#t = int(input())
+for i in range(t): TC()
+
+
+
+
+
