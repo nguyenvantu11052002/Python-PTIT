72ae89f71491e6a11238efab907fc14d85f445cc
Nguyễn Văn Tú <86882000+nguyenvantu11052002@users.noreply.github.com>
2022-12-03 23:44:25 +0700

    Update PYKT073 - XÁC ĐỊNH THỂ LOẠI THƠ.py

--- PYKT073 - XÁC ĐỊNH THỂ LOẠI THƠ.py
+++ PYKT073 - XÁC ĐỊNH THỂ LOẠI THƠ.py
@@ -6,12 +6,14 @@ def TC():
     for i in range(n):
         arr = input().split()
         a.append(len(arr))
-    res = 0
-    ok8 = True
+
     for i in range(len(a)):
         if a[i] == 6:
             a[i] = 8
+
     kq = []
+    res = 0 #dem so bai tho 68
+    ok8 = True
     for i in range(len(a)): #8888 7777 7777 888888
         if a[i] == 8 and ok8 == True:
             res += 1
@@ -20,6 +22,7 @@ def TC():
             if a[i] == 7 :
                 ok8 = True
     print(res + int(a.count(7) / 4))
+
     ok8 = True
     cnt = 0
     for i in range(len(a)):
@@ -35,7 +38,10 @@ def TC():
             if a[i] == 7:
                 cnt += 1
                 ok8 = True
-
+    if cnt > 0:
+        soBai = cnt // 4
+        for j in range(soBai):
+            kq.append(2)
     for i in kq:
         print(i)
 
@@ -44,18 +50,18 @@ t = 1
 for i in range(t): TC()
 
 # 12
-# Mot canh hai canh lai ba canh
-# Tran troc ban khoan giac chang lanh
-# Canh bon canh nam vua chop mat
-# Sao vang nam canh mong hon bay
-# Mot canh hai canh lai ba canh
-# Tran troc ban khoan giac chang lanh
-# Canh bon canh nam vua chop mat
-# Sao vang nam canh mong hon bay
 # Minh ve minh co nho ta
 # Muoi lam nam ay thiet tha man nong
 # Minh ve minh co nho khong
 # Nhin cay nho nui nhin song nho nguon
+# Mot canh hai canh lai ba canh
+# Tran troc ban khoan giac chang lanh
+# Canh bon canh nam vua chop mat
+# Sao vang nam canh mong hon bay
+# Mot canh hai canh lai ba canh
+# Tran troc ban khoan giac chang lanh
+# Canh bon canh nam vua chop mat
+# Sao vang nam canh mong hon bay
 
 
 
